dd9d99856aef3e1f199af857beebeab81c2721ec
lanternavermelha <f_figueira@hotmail.com>
2020-01-18 19:33:54 +0000

    created REST customer controller

---
+++ src/main/java/lanterna/vermelha/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,27 @@
+package lanterna.vermelha.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lanterna.vermelha.springdemo.entity.Customer;
+import lanterna.vermelha.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	//autowire the Customer Service
+	@Autowired
+	private CustomerService customerService;
+	
+	//add mapping fot GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+
+}
