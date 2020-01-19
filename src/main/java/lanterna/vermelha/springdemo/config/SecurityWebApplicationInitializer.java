c543e66a6636737ce697c337b72a60c3fb9f58fb
lanternavermelha <f_figueira@hotmail.com>
2020-01-19 20:22:55 +0000

    add spring security to CRM REST API

---
+++ src/main/java/lanterna/vermelha/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package lanterna.vermelha.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+					extends AbstractSecurityWebApplicationInitializer{
+
+}
