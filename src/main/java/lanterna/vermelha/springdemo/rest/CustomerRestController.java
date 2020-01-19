9328104aef9b8febcec84adf656b4d5bb93ff223
lanternavermelha <f_figueira@hotmail.com>
2020-01-19 15:24:08 +0000

    add customer with POST

--- src/main/java/lanterna/vermelha/springdemo/rest/CustomerRestController.java
+++ src/main/java/lanterna/vermelha/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,18 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
 		return customerService.getCustomer(customerId);
+	}
+	
+	//add mapping for POST /customer - add new customer
+	@PostMapping("/customer")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		//if the id is 0 the dao will insert a new customer (instead of update)
+		
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
 	}
 
 }
